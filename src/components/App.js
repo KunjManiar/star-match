59266e3372a1a2c1786c701e4749af07a0c38a46
Kunj <kunjmaniar1999@gmail.com>
2020-12-27 16:33:09 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
+
+import StarMatch from './StarMatch'
+
+
+
+
+
+
+// ReactDOM.render(<StarMatch />, mountNode);	
 
 export function App({ initialData }) {
-  const [count, setCount] = React.useState(0);
+  // const [count, setCount] = React.useState(0);
   return (
-    <div>
-      <h1>{initialData.appName}</h1>
-      This is a sample stateful and server-side rendered React application.
-      <br />
-      <br />
-      Here is a button that will track how many times you click it:
-      <br />
-      <br />
-      <button title="increment" onClick={() => setCount(count + 1)}>
-        {count}
-      </button>
-    </div>
+    <StarMatch />
+    // <div>
+    //   <h1>{initialData.appName}</h1>
+    //   This is a sample stateful and server-side rendered React application.
+    //   <br />
+    //   <br />
+    //   Here is a button that will track how many times you click it:
+    //   <br />
+    //   <br />
+    //   <button title="increment" onClick={() => setCount(count + 1)}>
+    //     {count}
+    //   </button>
+    // </div>
   );
 }
